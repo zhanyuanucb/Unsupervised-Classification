1e93b45cceb390aa7a5687acf372baab07c173d0
zhanyuanucb <zhang_zhanyuan@berkeley.edu>
2021-01-21 20:16:57 +0000

    add run_cluster.sh

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,6 +1,6 @@
-#python moco.py --config_env configs/env.yml --config_exp configs/pretext/moco_fashion.yml 
-#python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_fashion.yml
-#python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_fashion.yml
+python moco.py --config_env configs/env.yml --config_exp configs/pretext/moco_fashion.yml 
+python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_fashion.yml
+python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_fashion.yml
 
 python eval.py --config_exp configs/selflabel/selflabel_fashion.yml \
                --model fashion_results/fashion/selflabel/model.pth.tar \
